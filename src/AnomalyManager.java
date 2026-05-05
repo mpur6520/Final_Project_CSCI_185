d7f1a0fed6b068abbcbfcf488805532ecca8caad
maru <apuran263@gmail.com>
2026-05-05 13:58:32 -0400

    altered red light probability

--- src/AnomalyManager.java
+++ src/AnomalyManager.java
@@ -23,7 +23,7 @@ public class AnomalyManager {
         else if (baseRoll < 70) {
             panel.currentBase = BaseState.EYES;
         }
-        else if (baseRoll < 85) {
+        else if (baseRoll < 88) {
             panel.currentBase = BaseState.NO_BATHROOM;
         }
         else if (baseRoll < 98) {
